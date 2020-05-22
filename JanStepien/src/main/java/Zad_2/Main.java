517feaa1f8d07a3d275464d6e03b2fe7fbbc8614
Jan <janekk3@op.pl>
2020-05-22 18:31:04 +0200

    Zadanie 2

--- JanStepien/src/main/java/Zad_2/Main.java
+++ JanStepien/src/main/java/Zad_2/Main.java
@@ -12,13 +12,14 @@ package Zad_2;
 public class Main {
 
     public static void main(String[] args) {
+        //utorzenie i inicjalizacja zmiennych sdudent nauczyciel
         Teacher[] t = new Teacher[2];
         Student[] s = new Student[2];
         t[0] = new Teacher("Boss", 23, "Maths", "Janusz", "Dzida", "superlevel", 45);
         t[1] = new Teacher("mediumBoss", 15, "Phisic", "Julisz", "Kat", "highlevel", 30);
         s[0] = new Student("informatik", 666, 10, "Jako", "Tako", "medium", 15);
         s[1] = new Student("Medic", 333, 2, "Wanda", "Szymanek", "second", 20);
-        System.out.println(t[0].toString());
+        System.out.println(t[0].toString());// wyświetlenie wyników
         System.out.println(t[1].toString());
         System.out.println(s[0].toString());
         System.out.println(s[1].toString());
